44cdaebc95b6f30bb1504e87f93271716d00f505
bouzaziinesrine <bouzazinesrine10@gmail.com>
2023-07-29 20:15:58 +0100

    OK

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,18 +1,37 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react"
-
+import axios from 'axios'
+import { toast } from "react-hot-toast"
+import { useNavigate }   from "react-router-dom"
 
 
 export default function Register() {
-    const [data, setData]= useState({
+    const navigate = useNavigate()
 
+    const [data, setData]= useState({
         name:'',
         email:'',
         password:''
 
     })
 
-    const registerUser = (e) =>{
-        e.preventDefault()
+    const registerUser = async(e) =>{
+        e.preventDefault();
+        const {name, email, password} = data
+        try {
+            const {data} = await axios.post('register',{
+                name, email, password
+            })
+            if (data.error){
+                toast.error(data.error)
+            }else{
+                setData({})
+                toast.success('Login Successful. Welcome!')
+                navigate('/login')
+            }
+        } catch (error) {
+            console.log('error')
+        }
     }
 
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,95 @@
-
-
+const User = require('../models/user')
+const {hashPassword, comparePassword} = require ('../helpers/auth')
+const { copy } = require('../routes/authRoutes')
+const jwt = require('jsonwebtoken')
 
 const test = (req, res) => {
     res.json('test is working')
 }
 
+// Register Endpoint
+const registerUser = async (req, res) =>{
+    try {
+        const {name, email, password} = req.body;
+        // Check if name was entered
+        if (!name) {
+            return res.json({
+                error : 'name is required'
+            })
+        }
+        // Check if password is good
+        if(!password || password.length < 6){
+            return res.json({
+                error: 'password is required and should be at least 6 characters long'
+            })
+        }
+        // Check email
+        const exist = await User.findOne({email})
+        if(exist){
+            return res.json({
+                error:'Email is taken already'
+            })
+        }
+
+        const hashPassword = await hashPassword(password)
+        // Create user in database
+        const user = await User.create({
+            name, email, password:hashPassword
+        })
+        return res.json(user)
+
+    }catch(error){
+        console.log('error')
+    }
+}
+
+
+// Login Endpoint
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        // Check if user exists
+        const user = await User.findOne({email})
+        if(!user){
+            return res.json({
+                error: 'No user found'
+            })
+        }
+        // Check if password match
+        const match = await comparePassword(password, user.password)
+        if(match){
+            jwt.sign({email: user.email, name:  user.name, id: user._id}, process.en.JWT_SECRET, {},(err, tokrn) => {
+                if(err) throw err;
+                res.cookie('token',token).json(user)
+            })
+        }
+        if(!match){
+            res.json({
+                error:"Passwords do not match"
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+const getProfile = (req, res) => {
+    const {token} = req.cookies
+    if(token) {
+        jwt.verify(token,JWT_SECRET, process.env.JWT_SECRET, {}, (err, user) => {
+            if(err) throw err;
+                res.json(user)
+        })
+    }else {
+        res.json(null)
+    }
+
+}
 
 module.exports = {
-    test
-
+    test ,
+    registerUser ,
+    loginUser ,
+    getProfile
 }
